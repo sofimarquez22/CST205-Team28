bf9e14d4c316a140297dec73457543d0d5ba99c7
Eduardo Soto <esoto-rodriguez@csumb.edu>
2018-04-23 14:49:11 -0700

    testing for merge

--- homepage.py
+++ homepage.py
@@ -4,3 +4,7 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
 from image_information import image_info
 from PIL import Image
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
