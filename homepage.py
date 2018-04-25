4d38a90498f9a58b3726a8c31a416682285cbda8
Eduardo Soto <esoto-rodriguez@csumb.edu>
2018-04-25 15:05:40 -0700

    adjust homepage

--- homepage.py
+++ homepage.py
@@ -9,17 +9,24 @@ image_list = ['driveforest', 'icemountains', 'nightcity']
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('CST 205 Main Window')
+        self.setWindowTitle('Manipulaing')
 
         self.combo_box_images = QComboBox()
         self.combo_box_images.addItems(image_list)
         self.box_label = QLabel("")
+        self.first_title = QLabel('Choose an image')
 
-        vbox1 = QVBoxLayout()
-        vbox1.addWidget(self.combo_box_images)
-        vbox1.addWidget(self.box_label)
 
-        self.setLayout(vbox1)
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.first_title)
+        hbox1.addWidget(self.combo_box_images)
+        hbox1.addWidget(self.box_label)
+
+
+        # hbox2 = QHBoxLayout()
+
+
+        self.setLayout(hbox1)
         self.combo_box_images.currentIndexChanged.connect(self.update_ui)
         self.setWindowTitle("Window layout")
 
