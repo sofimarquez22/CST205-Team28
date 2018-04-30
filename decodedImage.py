d3a11247d9620115caf7ba00fe17b8c512f92f22
Sofia Marquez <sofiamarquez@Sofias-MacBook-Air.local>
2018-04-30 15:08:31 -0700

    Havent pulled in a while so thats what I am going

--- decodedImage.py
+++ decodedImage.py
@@ -1,30 +1,13 @@
-from PIL import Image
-def decode_image(img):
-    """
-    check the red portion of an image (r, g, b) tuple for
-    hidden message characters (ASCII values)
-    """
-    width, height = img.size
-    msg = ""
-    index = 0
-    for row in range(height):
-        for col in range(width):
-            try:
-                r, g, b = img.getpixel((col, row))
-            except ValueError:
-                # need to add transparency a for some .png files
-                r, g, b, a = img.getpixel((col, row))
-            # first pixel r value is length of message
-            if row == 0 and col == 0:
-                length = r
-            elif index <= length:
-                msg += chr(r)
-            index += 1
-    return msg
-# downloaded from internet web page
-encoded_image_file = "Beach7_enc.png"
-# get the hidden text back ...
-img2 = Image.open(encoded_image_file)
-print(img2, img2.mode)  # test
-hidden_text = decode_image(img2)
-print("Hidden text:\n{}".format(hidden_text))
+import sys
+import PyQt5.Qwidgets import QApplication, Qwidget, QLabel
+from PyQt5.QtGui import QPixmap
+#import PIL from Image
+app = QApplication(sys.argv)
+class Image(Qwidget):
+    def __init__(self):
+        self.setWindowTitle('Decoded Image')
+        self.my_image = QPixmap(#need a way to get the image from the previous page)
+        self.show()
+
+image = Image()
+sys.exit(app.exec_())
