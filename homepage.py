e79126f6138f0a0706ba4b5ec6d28dae6d24e29b
Eduardo Soto <esoto-rodriguez@csumb.edu>
2018-05-07 13:26:32 -0700

    Order the files

--- homepage.py
+++ homepage.py
@@ -5,7 +5,6 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QPixmap
 from PIL import Image
 
-# image_list = ['driveforest', 'icemountains', 'nightcity']
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -14,20 +13,9 @@ class MyWindow(QWidget):
         self.int_label = QLabel("How to use")
         self.int_button = QPushButton("Click Here!", self)
 
-        # self.combo_box_images = QComboBox()
-        # self.combo_box_images.addItems(image_list)
-        # self.box_label = QLabel("")
-        # self.first_title = QLabel('Choose an image')
-        # self.start_button = QPushButton("Start", self)
-        # self.start_button.clicked.connect(self.chosen_image)
-
-
         hbox1 = QHBoxLayout()
         hbox1.addWidget(self.int_label)
         hbox1.addWidget(self.int_button)
-        # hbox1.addWidget(self.first_title)
-        # hbox1.addWidget(self.combo_box_images)
-        # hbox1.addWidget(self.box_label)
 
         gbox1 = QGroupBox()
         gbox1.setLayout(hbox1)
@@ -36,48 +24,43 @@ class MyWindow(QWidget):
         self.search = QLineEdit()
         self.file_button = QPushButton("Search", self)
         self.file_button.clicked.connect(self.click)
+        self.dec_label = QLabel("Push this to Deconde")
+        self.dec_button = QPushButton("Decode", self)
+        self.dec_button.clicked.connect(self.click_dec)
 
         vbox2 = QVBoxLayout()
         vbox2.addWidget(self.choose_label)
         vbox2.addWidget(self.search)
         vbox2.addWidget(self.file_button)
-
+        vbox2.addWidget(self.dec_label)
+        vbox2.addWidget(self.dec_button)
 
         gbox2 = QGroupBox()
         gbox2.setLayout(vbox2)
-
 
         masterbox = QVBoxLayout()
         masterbox.addWidget(gbox1)
         masterbox.addWidget(gbox2)
 
-
-
         self.setGeometry(500, 500, 500, 500)
         self.setLayout(masterbox)
-        # self.combo_box_images.currentIndexChanged.connect(self.update_ui)
         self.setWindowTitle('Manipulaing')
 
 
     @pyqtSlot()
-    def update_ui(self):
-        my_text = self.combo_box_images.currentText()
-        my_index = self.combo_box_images.currentIndex()
-        self.box_label.setText(f'You chose {my_list[my_index]}.')
-
-    # def chosen_image(self):
-    #     encoded = self.encode_image(fileName)
-    #     self.openImage(encoded)
-    #     self.show()
-
     def click(self):
         self.title = 'PyQt5 file dialogs - pythonspot.com'
-        # self.openFileNameDialog()
         fileName = self.openFileNameDialog()
-        # self.openImage(fileName)
         encoded = self.encode_image(fileName)
         self.openImage(encoded)
         self.show()
+
+
+    def click_dec(self,secret):
+        self.title = 'PyQt5 file dialogs - pythonspot.com'
+        fileName = self.openFileNameDialog()
+        hidden_text = self.decode_image(fileName)
+        print("Hidden text:\n{}".format(hidden_text))
 
 
     def openFileNameDialog(self):
@@ -90,33 +73,54 @@ class MyWindow(QWidget):
 
 
     def encode_image(self, fileName):
-        msg = self.search.text()
-        img = Image.open(fileName)
+        message = self.search.text()
+        image = Image.open(fileName)
 
-        length = len(msg)
-        if length > 255:
-            print("text too long! (don't exeed 255 characters)")
+        length = len(message)
+        if length > 200:
+            print("Please enter a shorter message.")
             return False
-        if img.mode != 'RGB':
+        if image.mode != 'RGB':
             print("image mode needs to be RGB")
             return False
-        encoded = img.copy()
-        width, height = img.size
+        encoded = image.copy()
+        width, height = image.size
         index = 0
         for row in range(height):
             for col in range(width):
-                r, g, b = img.getpixel((col, row))
+                r, g, b = image.getpixel((col, row))
                 if row == 0 and col == 0 and index < length:
                     asc = length
                 elif index <= length:
-                    c = msg[index -1]
+                    c = message[index -1]
                     asc = ord(c)
                 else:
                     asc = r
                 encoded.putpixel((col, row), (asc, g , b))
                 index += 1
-        encoded = encoded.save('new_image.png')
+        encoded = encoded.save('secret_image.png')
         return encoded
+
+
+    def decode_image(self,fileName):
+        chosen_image = Image.open(fileName)
+        width, height = chosen_image.size
+        secret = ""
+        index = 0
+        for row in range(height):
+            for col in range(width):
+                try:
+                    r, g, b = chosen_image.getpixel((col, row))
+                except ValueError:
+                    r, g, b, a = chosen_image.getpixel((col, row))
+                if row == 0 and col == 0:
+                    length = r
+                elif index <= length:
+                    secret += chr(r)
+                index += 1
+        chosen_image.save('decode_image.jpg')
+        return secret
+
 
     def openImage(self, encoded):
         self.new_win = QWidget()
